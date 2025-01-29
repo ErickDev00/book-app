7c73c62e3597b37d5c0fff0926bf1afedb5d0275
Erick Rodrigues <profissional.erick@gmail.com>
2025-01-29 15:13:08 -0300

    icones e menu adicionados

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable react/react-in-jsx-scope */
 
 import "./App.css";
 import Logo from "./components/logo/logo";
+import perfil from "./imagens/perfil.svg";
+import sacola from "./imagens/sacola.svg";
+
+const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "MINHA ESTANTE"];
+const icones = [sacola, perfil];
 
 function App() {
   return (
@@ -9,6 +15,21 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Logo></Logo>
+          <ul className="opcoes">
+            {textoOpcoes.map((texto) => (
+              <li className="opcao">
+                <p>{texto}</p>
+              </li>
+            ))}
+          </ul>
+
+          <ul className="icones">
+            {icones.map((icone) => (
+              <li className="icone">
+                <img src={icone} />
+              </li>
+            ))}
+          </ul>
         </header>
       </div>
     </>
